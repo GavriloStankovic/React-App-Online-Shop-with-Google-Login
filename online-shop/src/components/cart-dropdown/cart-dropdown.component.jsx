460cf0a1c1a0d7cbda0e52a05abf07132f490229
Gavrilo Stankovic <stakovic.gavrilo@gmail.com>
2023-05-24 02:58:13 +0200

    Added Checkout and Checkout Item components with total balance

--- online-shop/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ online-shop/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,11 +3,18 @@ import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
+import { useNavigate } from "react-router-dom";
 
 import React from "react";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -15,7 +22,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} CartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
--- online-shop/src/components/cart-item/cart-item.component.jsx
+++ online-shop/src/components/cart-item/cart-item.component.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import "./cart-item.style.scss";
 
 const CartItem = ({ CartItem }) => {
-  const { name, quantity } = CartItem;
+  const { name, quantity, imageUrl, price } = CartItem;
   console.log(CartItem);
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity}* $ {price}
+        </span>
+      </div>
     </div>
   );
 };
